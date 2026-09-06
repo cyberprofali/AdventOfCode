c359405c22184a366db5ffe48c835b0f94fe32cc
cyberprofali <aliev.mordecai@gmail.com>
2026-09-06 15:37:04 -0500

    Adding part2, will figure this out later

--- 2015/day-05/part2.c
+++ 2015/day-05/part2.c
@@ -7,7 +7,7 @@ int main() {
     char line[256];
     int num_nice_string = 0;
 
-    FILE = *in_file = fopen("input.txt", "r");
+    FILE *in_file = fopen("input.txt", "r");
 
     if(in_file == NULL) {
         printf("Error! Could not open file!\n"); 
@@ -15,22 +15,45 @@ int main() {
     }
 
     while(fgets(line, sizeof(line), in_file)) {
-    int pairing_count = 0;
-    int repearing_count = 0;
 
-    size_t len = strlen(line); 
-    
-    if(line[len - 1] == '\n') { 
-        line[len - 1] = '\0';
-        len--;
+        int pairing_count = 0;
+        int reapearing_count = 0;
+
+        size_t len = strlen(line); 
+
+        if(line[len - 1] == '\n') { 
+            line[len - 1] = '\0';
+            len--;
+        }
+
+        for(int i = 0; i < len; i++) { 
+            if(len < 3) {
+                break;
+            }
+            char ch1 = line[i];
+            char ch2 = line[i+1];
+            char ch3 = line[i+2];
+            
+            if(ch1 == ch3 && ch1 != ch2) { 
+                pairing_count++;
+            }
+            
+            for(int j = 2; j < len - 2; j++) { 
+                char ch4 = line[j];
+                char ch5 = line[j+1];
+                if(ch1 == ch4 && ch2 == ch5) {
+                    reapearing_count++;
+                } 
+            }
+        }
+        if(reapearing_count > 0 && pairing_count > 0) {
+            num_nice_string++;
+            reapearing_count = 0;
+            pairing_count = 0;
+        }
+
     }
-
-    for(int i = 0; i < len; i++) { 
-        char ch = line[i];
-
-
-    }
-
-    }
+    printf("The number of nice strings is: %i", num_nice_string);   
 }
+
 
